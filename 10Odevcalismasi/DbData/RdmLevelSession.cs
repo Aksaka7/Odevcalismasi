b8e043ffd428be8eeb036c2725d6a5c1c56c42b9
Mehmetasker <31123405+Aksaka7@users.noreply.github.com>
2024-01-10 20:17:18 +0300

    çalışma tamamlandı

---
+++ 10Odevcalismasi/DbData/RdmLevelSession.cs
@@ -0,0 +1,12 @@
+﻿using _10Odevcalismasi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace _10Odevcalismasi.DbData
+{
+    public class RdmLevelSession(DbContextOptions<RdmLevelSession> options) : DbContext(options)
+    {
+
+        //c# dbdeki tabloya ulasmak için bunu belirledim. 
+        public DbSet<Customers> Customers { get; set; }
+    }
+}
---
+++ 10Odevcalismasi/Models/Entities/Customers.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _10Odevcalismasi.Models.Entities
+{
+    public class Customers
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Adres { get; set; }
+        public int SponsorID { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
