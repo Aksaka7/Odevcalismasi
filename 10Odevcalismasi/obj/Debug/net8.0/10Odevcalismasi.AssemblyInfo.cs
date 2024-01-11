a1a078b33b0c24f3db431e762f65c805b874f1f9
Mehmetasker <31123405+Aksaka7@users.noreply.github.com>
2024-01-11 19:09:51 +0300

    test

--- 10Odevcalismasi/obj/Debug/net8.0/10Odevcalismasi.AssemblyInfo.cs
+++ 10Odevcalismasi/obj/Debug/net8.0/10Odevcalismasi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("10Odevcalismasi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e043ffd428be8eeb036c2725d6a5c1c56c42b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("10Odevcalismasi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("10Odevcalismasi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
